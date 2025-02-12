8e3514ca1e397b59a583b0b63329f8ce5dfecb90
itsmeavanish <avanishupadhyay633@gmail.com>
2025-02-12 23:47:47 +0530

    Your commit message

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,16 @@
+const mongoose=require("mongoose");
+require("dotenv").config();
+exports.connect=async ()=> {
+    const uri = process.env.MONGODB_URL; // Ensure this is defined
+    console.log(uri)
+    if (!uri) {
+      throw new Error('MongoDB URI is not defined');
+    }
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected successfully');
+    
+}
 
-const mongoose = require('mongoose');
-
-const connect = async () => {
-  const uri = process.env.MONGO_URI; // Ensure this is defined
-  if (!uri) {
-    throw new Error('MongoDB URI is not defined');
-  }
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log('Database connected successfully');
-};
-
-module.exports = connect;
