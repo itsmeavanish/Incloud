f3dc7d8fbf7812bdc454f87352e032fd819d8aa2
itsmeavanish <avanishupadhyay633@gmail.com>
2025-02-12 12:12:19 +0530

    Your commit message

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
 exports.connect=async ()=> {
-    const uri=process.env.MONGODB_URL;
+    const uri="mongodb+srv://avanishupadhyay633:CwXjVf586oQTfRCd@cluster0.c80t1.mongodb.net/mediauploader";
     console.log(uri);
     await mongoose
     .connect(uri,{
