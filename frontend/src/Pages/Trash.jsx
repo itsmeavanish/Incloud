fc095ed77e215f09e079936a27d074bd77b9fab6
itsmeavanish <avanishupadhyay633@gmail.com>
2025-02-27 19:41:59 +0530

    Your commit

--- frontend/src/Pages/Trash.jsx
+++ frontend/src/Pages/Trash.jsx
@@ -1,18 +1,48 @@
 import React from 'react'
-import { useAuth } from '../Context/useAuth'
-
+import[] { useAuth } from '../Context/useAuth'
+import styles from "../components/Content.module.css"
+import { SiFiles } from "react-icons/si";
+import { func } from 'prop-types'
 export default function Trash() {
   const {trash,trashData}=useAuth();
+  const formatDate = (dateString) => new Date(dateString).toLocaleDateString();
   return (
-    <div style={{textAlign:"center"}}>
-      {trash ?
-      <div>
-        {
-          trashData.map((item)=><div key={item._id}>{item.name}</div>)
-        }
-      </div>
-      :<span>No Items in Your Trash</span>}
+    
 
+    <ul className='display flex flex-col'>
+      <li className="flex justify-between p-4 text-gray-600 text-center border-b">
+          <span>Name</span>
+          <span className="pl-16">Type</span>
+          <span className="pl-8">FileSize</span>
+          <span>Created at</span>
+      </li>
+      { trashData.map((item)=><TrashBox key={item?._id} item={item}/>)}
+      
+
+      
+      
+
+    </ul>
+  )
+}
+function TrashBox({item}){
+  return(
+    <li 
+    className={styles.fileitem} 
+    onClick={handleevent}
+  >
+    <div className={styles.fileinfo}>
+      <span className={styles.fileicon}>
+        <SiFiles color="#fdca13" />
+      </span>
+      <span className={styles.filename}>{item.name}</span>
     </div>
+    <span className={styles.filetype}>Folder</span>
+    <span className={styles.filesize}>{fileSize} MB</span>
+    <span className={styles.filedate}>{formatDate(item?.createdAt)}</span>
+
+    {/* Hover content */}
+    
+  </li>
   )
 }
