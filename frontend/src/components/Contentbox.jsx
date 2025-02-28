b3d7a1a7ef0c23049606111b030789a4fdef6e98
itsmeavanish <avanishupadhyay633@gmail.com>
2025-02-28 19:48:24 +0530

    Your commit

--- frontend/src/components/Contentbox.jsx
+++ frontend/src/components/Contentbox.jsx
@@ -45,7 +45,7 @@ export default function Contentbox({ item,setvisible,visible,seturl,iframe,setif
         {/* Hover content */}
         
       </li>
-      <div className="w-fit h-fit bg-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center rounded-lg p-3 gap-4 relative  bottom-20" style={{left:"82%"}}>
+      <div className="w-fit h-fit bg-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center rounded-lg p-3 gap-4 relative  bottom-20" style={{left:"78%"}}>
           <span   onClick={()=>handlelinks(id)}>
           <RiLinksFill  />
           </span>
@@ -54,7 +54,7 @@ export default function Contentbox({ item,setvisible,visible,seturl,iframe,setif
         <Heart onClick={toggleFavorite}
         size={16}
         className={`transition-all ${
-          isFavorited ? "fill-current scale-125 rounded-full bg-red-500 text-red-500" : "scale-100 bg-transparent text-gray-700 rounded-full"
+          isFavorited ? "fill-current scale-125 rounded-full bg-transparent text-red-500" : "scale-100 bg-transparent text-gray-700 rounded-full"
         }`}
       />
     </button>
