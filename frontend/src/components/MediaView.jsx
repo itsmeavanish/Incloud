a01a9b51832cfa2286d71ec654fe003823cc44f2
itsmeavanish <avanishupadhyay633@gmail.com>
2025-03-03 16:27:39 +0530

    Your commit

--- frontend/src/components/MediaView.jsx
+++ frontend/src/components/MediaView.jsx
@@ -4,7 +4,9 @@ import { TbMaximize } from 'react-icons/tb';
 
 export default function MediaView({divRef,setIframe,iframe,url}) {
   const [visible,setVisible]=useState(true);
-    const enterFullscreen = () => {
+  const type=url?.split(".");
+  const length=type.length();
+      const enterFullscreen = () => {
         if (divRef.current) {
           if (divRef.current.requestFullscreen) {
             divRef.current.requestFullscreen();
@@ -48,11 +50,17 @@ export default function MediaView({divRef,setIframe,iframe,url}) {
             <IoMdCloseCircle />
           </span>
           <div style={{height:"100%", width:"100%", display:"flex", justifyContent:"center", alignItems:"center" }}>
+         
           {!iframe ? (
             <img src={url} height="70%" width="50%" alt="Preview" />
-          ) : (
-            <iframe src={url} height="100%" width="50%"title="Preview" style={{overflow:"hidden", scrollbarWidth:"none"}}></iframe>
-          )}
+          ) : 
+            type[length-1]==="pdf "? (<object data="https://res.cloudinary.com/dfstpdwih/raw/upload/v1740998610/Codehelp/tmp-2-1740998606384" 
+              type="application/pdf" 
+              width="100%" height="600px">
+          <p>Your browser does not support PDFs. <a href="https://res.cloudinary.com/dfstpdwih/raw/upload/v1740998610/Codehelp/tmp-2-1740998606384">Download PDF</a>.</p>
+      </object> 
+      ):<iframe src={url} height="100%" width="50%"title="Preview" style={{overflow:"hidden", scrollbarWidth:"none"}}></iframe>
+          }
           </div>
         </div>}
         </>
