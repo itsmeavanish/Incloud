88d5b0d70707713111b4361baa92c8322008c0dc
itsmeavanish <avanishupadhyay633@gmail.com>
2025-03-09 13:45:37 +0530

    Your commit

--- frontend/src/components/MediaView.jsx
+++ frontend/src/components/MediaView.jsx
@@ -4,8 +4,6 @@ import { TbMaximize } from 'react-icons/tb';
 
 export default function MediaView({divRef,setIframe,iframe,url}) {
   const [visible,setVisible]=useState(true);
-  const type=url?.split(".");
-  const length=type.length();
       const enterFullscreen = () => {
         if (divRef.current) {
           if (divRef.current.requestFullscreen) {
@@ -53,12 +51,6 @@ export default function MediaView({divRef,setIframe,iframe,url}) {
          
           {!iframe ? (
             <img src={url} height="70%" width="50%" alt="Preview" />
-          ) : 
-            type[length-1]==="pdf "? (<object data="https://res.cloudinary.com/dfstpdwih/raw/upload/v1740998610/Codehelp/tmp-2-1740998606384" 
-              type="application/pdf" 
-              width="100%" height="600px">
-          <p>Your browser does not support PDFs. <a href="https://res.cloudinary.com/dfstpdwih/raw/upload/v1740998610/Codehelp/tmp-2-1740998606384">Download PDF</a>.</p>
-      </object> 
       ):<iframe src={url} height="100%" width="50%"title="Preview" style={{overflow:"hidden", scrollbarWidth:"none"}}></iframe>
           }
           </div>
